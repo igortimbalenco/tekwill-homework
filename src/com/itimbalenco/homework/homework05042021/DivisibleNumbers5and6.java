a6679642d1fe3a1359e9281609ab9e5d1b527bdc
Igor <igor.timbalenco@gmail.com>
2021-04-06 21:42:18 +0300

    a little correction for previous commit

--- src/com/itimbalenco/homework/homework05042021/DivisibleNumbers5and6.java
+++ src/com/itimbalenco/homework/homework05042021/DivisibleNumbers5and6.java
@@ -2,9 +2,16 @@ package com.itimbalenco.homework.homework05042021;
 
 public class DivisibleNumbers5and6 {
     public static void main(String[] args) {
+        int counter = 0;
         for (int i = 100; i <= 1000; i++){
-            if ((i % 5 == 0)&&(i % 6 == 0))
+            if ((i % 5 == 0)&&(i % 6 == 0)){
                 System.out.print(i + " ");
+                counter++;
+                if (counter % 10 == 0){
+                    System.out.println();
+                }
+
+            }
         }
     }
 }
